a7d0bac03f986f02a61ee7c3d3cb2b3fc94c9341
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-06-07 00:19:12 -0700

    initial commit

---
+++ packages/create-proxy-app/templates/default/public/index.js
@@ -0,0 +1,71 @@
+/** @type {typeof import("@mercuryworkshop/scramjet-utils")} */
+/** @type {typeof import("@mercuryworkshop/scramjet")} */
+
+const form = document.getElementById("sj-form");
+const address = document.getElementById("sj-address");
+const frameWrapper = document.getElementById("sj-frame-wrapper");
+const frameElement = document.getElementById("sj-frame");
+const error = document.getElementById("sj-error");
+const errorCode = document.getElementById("sj-error-code");
+
+let controller;
+let frame;
+async function init() {
+	controller = await initBootstrap();
+
+	const cachePlugin = new $scramjetUtils.HttpCachePlugin();
+	const urlWatcher = new $scramjetUtils.UrlWatcherPlugin((url) => {
+		address.value = url;
+	});
+	const catchEscapedLinks = new $scramjetUtils.CatchEscapedLinksPlugin(
+		(url) => new URL(`/?goto=${encodeURIComponent(url.href)}`, location.origin)
+	);
+
+	frame = controller.createFrame(frameElement, {
+		plugins: [cachePlugin, urlWatcher, catchEscapedLinks],
+	});
+
+	installPlugins(frame);
+}
+
+function installPlugins(frame) {
+	const errorPlugin = new $scramjet.Plugin("error-handler");
+	errorPlugin.tap(frame.hooks.error.request, (context, props) => {
+		props.suppressError = false;
+		console.error(context.error);
+		showErrorScreen(
+			`Scramjet couldn't load ${context.rawrequest.rawUrl}`,
+			context?.error?.message || context?.error?.toString() || "Unknown error"
+		);
+	});
+}
+
+function showErrorScreen(error, details) {
+	frameWrapper.style.display = "none";
+	error.textContent = error;
+	errorCode.textContent = details;
+}
+
+async function navigate(url) {
+	if (!frame || !controller) {
+		await init();
+	}
+	await frame.go(url);
+	frameWrapper.style.display = "flex";
+}
+
+form.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	try {
+		await navigate(address.value);
+	} catch (error) {
+		showErrorScreen(error.message, error.stack);
+	}
+});
+
+const goto = new URL(location.href).searchParams.get("goto");
+if (goto) {
+	history.replaceState(null, "", location.pathname);
+	address.value = goto;
+	navigate(goto).catch((error) => showErrorScreen(error.message, error.stack));
+}
